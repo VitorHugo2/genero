b07252fad2b036783a62aa4b3beab97303ca74fa
konaly <vitorkonalyblack@gmail.com>
2022-11-09 01:11:56 -0300

    add.link-imgs+func

---
+++ index.js
@@ -0,0 +1,39 @@
+let imgs = {
+    
+    "criança":{
+        "m":["https://uploads.metropoles.com/wp-content/uploads/2019/01/25195050/iStock-521262996.jpg","https://th.bing.com/th/id/R.40aaecd1751b6fab3b59ebe7fe5aeccd?rik=US%2b570jruFpsCw&riu=http%3a%2f%2felfredadalby.com%2fwp-content%2fuploads%2f2016%2f06%2f02-11604-post%2fJ6A0065(pp_w768_h512).jpg&ehk=SIAqaGudOKMWCsLA31xTfmWGmMTDbRgQU88AmkS6nYk%3d&risl=&pid=ImgRaw&r=0","https://th.bing.com/th/id/R.f07f0e2b2f83530435e95ebe05c3ec1e?rik=2i%2fGvnzPw7%2bScg&riu=http%3a%2f%2ffunds.gfmcdn.com%2f6881269_1447352117.1797.jpg&ehk=ukH2O9vNRB5h6WCPi%2b%2fNBqzUbAGWO%2b9RlncPHM8pYP8%3d&risl=&pid=ImgRaw&r=0",],
+        "f":["https://th.bing.com/th/id/OIP.sRsYdroTi0EEDuuxn-n23QAAAA?pid=ImgDet&w=400&h=266&rs=1","https://th.bing.com/th/id/R.2767dd684da38360fa62756e05ec3044?rik=V59VJ4dMR9Rv%2fg&riu=http%3a%2f%2fwww.beautifulblackhairandskin.com%2fwp-content%2fuploads%2f2015%2f06%2fKidsAfroPuffs10.jpg&ehk=0%2bQThCyc88fG%2bGlQBlJmRmiy3etJ0Ar9iILnXUSarn4%3d&risl=&pid=ImgRaw&r=0","https://i0.wp.com/www.geledes.org.br/wp-content/uploads/2016/10/dia-das-criancas-negras001.jpg?ssl=1",],
+        "gn":["https://th.bing.com/th/id/OIP.wJ5v7POT0ucl5ljg6V6cxAHaFj?pid=ImgDet&rs=1","https://cdn.awsli.com.br/800x800/1093/1093976/produto/49851435/94e4979876.jpg","https://th.bing.com/th/id/OIP.VSaWTlLcK2NqlrOpBpPy6AHaHa?pid=ImgDet&rs=1"],
+    },
+    "jovem":{
+        "m":["https://th.bing.com/th/id/R.65d221ca737d0df4397a0b239066a640?rik=P7Nr%2fUSL3baVmA&riu=http%3a%2f%2fbi.gazeta.pl%2fim%2fd5%2f16%2f14%2fz21064149V%2cProbne-egzaminy-gimnazjalne.jpg&ehk=PMaRj8GmWGZrFKKVCkWaXp7Ghcj%2bA0R2EyqRWGYJEKs%3d&risl=&pid=ImgRaw&r=0","https://www.studentthinkbox.com/wp-content/uploads/2017/05/students_corner-600x902.jpg","https://img.freepik.com/vrije-photo/jonge-studentenmens-die-op-witte-achtergrond-wordt-geisoleerd-die-nummer-tien-met-handen-toont_1187-138170.jpg?size=626&ext=jpg",],
+        "f":["https://www.solver-rh.com.br/wp-content/uploads/2020/01/2020-01-14-interessando-se-em-contratar-jovem-aprendiz-802x506.jpg","https://th.bing.com/th/id/OIP.R2ImMf8Wk-0H--pGnkNpewHaGk?pid=ImgDet&rs=1","https://th.bing.com/th/id/OIP.3r_z7j-tf8QXFH5huXqHmwAAAA?pid=ImgDet&rs=1",],
+        "gn":["https://data.whicdn.com/images/334707933/original.jpg","https://images.uncyc.org/pt/thumb/7/7c/Fodase16.jpg/230px-Fodase16.jpg","https://www.xgrupos.com/img-400.400.100-b4d0b26e42460393c0c4178c944a68ec.jpg",],
+    },
+    "adulto":{
+        "m":["https://img.freepik.com/fotos-premium/jovem-adulto-bonito-sorrindo-parecendo-feliz-positivo-confiante-e-bem-sucedido-com-os-dois-polegares-para-cima_1194-215353.jpg","https://sorridents.com.br/wp-content/uploads/2017/01/aparelho-ortodontico-para-adultos-por-que-usar-768x572.jpeg","https://m.extra.globo.com/incoming/24459320-9c6-c58/w367h550-PROP/luccas-neto.jpg",],
+        "f":["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRUirhWhmFuv98UbyNylnbGNWQefF8eEzWAIg&usqp=CAU","https://s3-sa-east-1.amazonaws.com/doctoralia.com.br/doctor/148b8e/148b8e7e0f2e99d7d63fa38def1c6d32_large.jpg","https://s3-sa-east-1.amazonaws.com/doctoralia.com.br/doctor/0f7394/0f7394a32a19612c1925df109d06528d_large.jpg"],
+        "gn":["https://avowhcbwko.cloudimg.io/v7/https://empreender.nyc3.cdn.digitaloceanspaces.com/grupowhats/8cbc15c172f8d7a7cd0c068d328c0861?w=400&org_if_sml=1","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_nSZOL5I3GmG7PQoFGgLcCLEpr07anViuOVcS8U3a4n8B1UJydTuKtVSb973yxWkrPps&usqp=CAU","https://us-tuna-sounds-images.voicemod.net/aff557a2-c579-43fb-a1b7-64412601c8d2-1665039970168.png",],
+    },
+    "idoso":{
+        "m":["https://i.pinimg.com/474x/e3/68/07/e3680777826c76703ccd69b7bdff99d2.jpg","https://wp.ebradi.com.br/wp-content/uploads/2021/01/ebradi_ebradi_image_707-1080x675.jpeg","https://corasenior.com.br/wp-content/uploads/elementor/thumbs/idoso-sociedade-cora-residencial-para-idosos-ova5juvui2agrp7ez89ttb61pnwqs5wx52tykaeyjc.jpg",],
+        "f":["https://prefeitura.pbh.gov.br/sites/default/files/noticia/img/Pessoa%20idosa%20zaira-magalhaes-pbh%20%2898%29.png","https://institutodelongevidademag.org/storage/app/media/2022/08.AGO/estatuto-da-pessoa-idosa-instituto-de-longevidade-mag-meio.jpg","https://metodosupera.com.br/wp-content/uploads/2021/09/No-Dia-do-Idoso-5-dicas-para-abandonar-o-preconceito-a-pessoas-com-mais-de-60-anos_SUPERA-Ginastica-para-o-cerebro_.jpg",],
+        "gn":["https://imageproxy.ifunny.co/crop:x-20,resize:640x,quality:90x75/images/8da1004d8aebabc1e957ba53052f9923e75c1834b79da331c551226d8424c92e_1.jpg","https://cdn.statically.io/img/cdn.solion.app/q=80,f=auto,w=330/images/GjEhF1Xgu05DzlF4HXH04T.jpg","https://pbs.twimg.com/profile_images/1529487153402724352/ORsFoDlm_400x400.jpg"],
+    },
+    "anciao":{
+        "m":["https://midias.agazeta.com.br/2020/08/27/-lancamento-da-pedra-presidente-jair-bolsonaro-no-lancamento-da-pedra-fundamental-para-a-duplicacao-da-br---469-em-foz-do-iguacu-307725-article.jpg","https://images3.memedroid.com/images/UPLOADED73/5e12bd58237a7.jpeg","https://www.folhavitoria.com.br/geral/blogs/portugues-em-dia/wp-content/uploads/2021/07/208.-Anciao.jpg",],
+        "f":["https://s2.glbimg.com/rdn8PgwKyQc7spRfZGhc4Rr2pv4=/0x0:312x471/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2020/9/T/DUu3WlQYS6mDzkPZSQyg/teresa.jpg","http://memorialdaresistenciasp.org.br/wp-content/uploads/tainacan-items/3161/50944/DilmaRousseff-scaled.jpg","https://img.freepik.com/fotos-premium/retrato-de-uma-velha-segurando-dinheiro-isolado_246836-889.jpg"],
+        "gn":["",],
+    },
+}
+
+function enviar(){
+    var imag = document.getElementById("imagem").src
+    var mensagem = document.getElementById("mensagem")
+    var ano = new Date().getFullYear()
+    var fano = document.getElementById("txtano").value
+    if(fano.length <= 0 || fano > ano){
+        alert("Verifique os campos acima e tente novamente!")
+    }
+    
+}
